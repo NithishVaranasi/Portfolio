9d3e4e6b84f04928234854517188d6ec74bc922c
NithishVaranasi <92880413+NithishVaranasi@users.noreply.github.com>
2024-03-29 01:57:08 +0530

    chnages Form

--- portfolio/src/components/Form.jsx
+++ portfolio/src/components/Form.jsx
@@ -1,9 +1,10 @@
-import "./FormStyles.css"
-import React from 'react'
+import "./FormStyles.css";
+import React from 'react';
 
 const Form = () => {
-  return <div className="form">
-    <form action="https://formspree.io/f/myyrnjyg" method="POST">
+  return (
+    <div className="form">
+      <form action="https://formspree.io/f/myyrnjyg" method="POST">
         <label>Your Name</label>
         <input type="text" required />
         <label>Email</label>
@@ -13,7 +14,9 @@ const Form = () => {
         <label>Feedback</label>
         <textarea rows="10" placeholder="Type your feedback" required/>
         <button className="btn">Submit</button>
-    </form>
-  </div>;
+      </form>
+    </div>
+  );
 };
-export default Form
+
+export default Form;
