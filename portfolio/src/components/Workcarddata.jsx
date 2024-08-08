e8d3bfdbe032674b2e273a77f294f76eaffd52eb
NithishVaranasi <92880413+NithishVaranasi@users.noreply.github.com>
2024-08-09 00:39:26 +0530

    text

--- portfolio/src/components/Workcarddata.jsx
+++ portfolio/src/components/Workcarddata.jsx
@@ -5,21 +5,21 @@ const ProjectCardData=[
     {
         imgsrc: pro1,
         title:"Online Payments Fraud Transaction Detection",
-        text: "This project describes about detecting the fraud online payment transactions in our day-to-day life. In this, I have used some advanced machine learning techniques. Additionally, I designed a website to predict whether the transaction is fraud or not.",
+        text:"This project involves detecting fraudulent online payments using advanced machine learning techniques. I also created a website to predict if a transaction is fraudulent or not.", 
         view:"kaggle.com",
         source:"https://github.com/NithishVaranasi/ML_DL.git"
     },
     {
         imgsrc: pro2,
         title:"Temperature",
-        text: " This Project describes about weather in various locations from all over the world. Technologies that i have used JavaScript with React Native .",
+        text:"This project showcases weather information from various global locations. Technologies used include JavaScript and React Native.",
         view:"androidstudio.com",
         source:"https://github.com/NithishVaranasi/ReactNative.git" 
     },
     {
         imgsrc: pro3,
         title:"Alarm Based Driver Drowsiness Detection",
-        text: "This project focuses on detecting whether a driver is experiencing drowsiness or not.I have utilized advanced Deep Learning techniques and Image Processing methods. Additionally, I have designed a Graphical User Interface (GUI) using various Python modules.",
+        text: "This project detects driver drowsiness using advanced Deep Learning and Image Processing techniques. I also designed a user-friendly Graphical User Interface (GUI) with Python.",
         view:"",
         source:"https://github.com/NithishVaranasi/ML_DL.git" 
     }
