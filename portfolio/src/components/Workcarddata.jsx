ba9593ddd38b5f359c34887ec7f235cd3c45f4f8
NithishVaranasi <92880413+NithishVaranasi@users.noreply.github.com>
2024-08-09 01:27:00 +0530

    edit

--- portfolio/src/components/Workcarddata.jsx
+++ portfolio/src/components/Workcarddata.jsx
@@ -1,23 +1,23 @@
-import pro1 from"../assets/pro1.jpg"
-import pro2 from"../assets/pro2.jpg"
-import pro3 from"../assets/pro3.jpg"
+import Pro1 from"../assets/Pro1.jpg"
+import Pro2 from"../assets/Pro2.jpg"
+import Pro3 from"../assets/Pro3.jpg"
 const ProjectCardData=[
     {
-        imgsrc: pro1,
+        imgsrc: Pro1,
         title:"Online Payments Fraud Transaction Detection",
         text:"This project involves detecting fraudulent online payments using advanced machine learning techniques. I also created a website to predict if a transaction is fraudulent or not.", 
         view:"kaggle.com",
         source:"https://github.com/NithishVaranasi/ML_DL.git"
     },
     {
-        imgsrc: pro2,
+        imgsrc: Pro2,
         title:"Temperature",
         text:"This project showcases weather information from various global locations. Technologies used include JavaScript and React Native.",
         view:"androidstudio.com",
         source:"https://github.com/NithishVaranasi/ReactNative.git" 
     },
     {
-        imgsrc: pro3,
+        imgsrc: Pro3,
         title:"Alarm Based Driver Drowsiness Detection",
         text: "This project detects driver drowsiness using advanced Deep Learning and Image Processing techniques. I also designed a user-friendly Graphical User Interface (GUI) with Python.",
         view:"",
